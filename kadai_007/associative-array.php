18deb9633e29b9a1003bf136156263f6d65a16f0
kenty-25 <142951227+kenty-25@users.noreply.github.com>
2024-08-20 01:27:51 -0700

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 値の代入
+    $item = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 値を出力する
+    print_r($item);
+    ?>
+  </p>
+</body>
+
+</html>
